5b8841e9df3da14764c040e307a384a779fa9577
Kaviya12355 <118159148+Kaviya12355@users.noreply.github.com>
2024-02-05 10:03:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/servo1/ui/home/Loginpage.java
@@ -0,0 +1,15 @@
+package com.example.servo1.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.servo1.R;
+
+public class Loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage2);
+    }
+}
